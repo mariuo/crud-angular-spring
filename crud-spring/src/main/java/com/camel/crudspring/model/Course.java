5524945de17e9940afb52d5d3f845723d25ad1c0
Mario Camelo <mariuo@gmail.com>
2024-04-23 15:39:33 -0400

    spring: Created entity, controller, repository

---
+++ crud-spring/src/main/java/com/camel/crudspring/model/Course.java
@@ -0,0 +1,26 @@
+package com.camel.crudspring.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String category;
+
+}
